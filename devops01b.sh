6fd56f0d932abf68eb7cc4e0b4d75956d216978e
Raghwendra Pandey <pandey.raghwendra@gmail.com>
2019-05-15 14:58:17 -0700

    First Commit for Assignment 2

---
+++ devops01b.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Enter the system information you want to see, options are :- volumes cpu ram network all "
+read answer
+if [ "$answer" = 'volumes' ] ;then
+    echo "Here is the Volume Information"
+    echo "`df -h`"
+elif [ "$answer" = 'cpu' ] ;then
+    echo "Here is the CPU Information"
+    echo "`system_profiler SPHardwareDataType`"
+elif [ "$answer" = 'ram' ] ;then
+    echo "Here is the Ram Information"
+    echo "`system_profiler SPHardwareDataType`"
+elif [ "$answer" = 'network' ] ;then
+    echo "Here is the Network Information"
+    echo " Mac Address is :- "
+    echo "`networksetup -listallhardwareports`"
+    echo " IP Address is :- "
+    echo "`ipconfig getifaddr en0`"
+else
+    echo "No"
+fi
